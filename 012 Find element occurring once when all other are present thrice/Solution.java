40634c1c88bf94ef7bff8f10ff42e4ad3905ea18
Parth Bhalerao <98455586+pvbgeek@users.noreply.github.com>
2022-07-25 21:41:26 +0530

    Create Solution.java

---
+++ 012 Find element occurring once when all other are present thrice/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    static int singleElement(int[] arr , int N) {
+        // code here
+        int ones = 0, twos = 0;
+        int common_bit_mask;
+        for (int i = 0; i < N; i++) 
+        {
+            twos = twos | (ones & arr[i]);
+            ones = ones ^ arr[i];
+            common_bit_mask = ~(ones & twos);
+            ones &= common_bit_mask;
+            twos &= common_bit_mask;
+        }
+        return ones;
+    }
+}

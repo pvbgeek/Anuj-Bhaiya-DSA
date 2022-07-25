e414dd920407608164c10973a4212a69307a3fea
Parth Bhalerao <98455586+pvbgeek@users.noreply.github.com>
2022-07-25 20:36:35 +0530

    Add files via upload

---
+++ 007 BitChangesRequired/Main.java
@@ -0,0 +1,18 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+    
+    	// Given two numbers , we need to find number of changes required to be done in number 'a' to change it to number 'b'
+        int a = 6; // Initial Number
+        int b = 5; // Final Number
+        int cnt = 0 ; // Count of changes
+        int n = a^b;
+        while(n!=0)
+        {
+            cnt++;
+            n = n & (n-1);
+        }
+        System.out.println("Number of Changes required in bits to change from a to b is : " + cnt);
+    }
+}

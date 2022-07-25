7e5504f24d94bd2705ba3b123662907e26b23b30
Parth Bhalerao <98455586+pvbgeek@users.noreply.github.com>
2022-07-25 20:14:12 +0530

    Add files via upload

---
+++ 001 Multiply&DivideBy2-BitwiseOperator/Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int a = 6;
+        int b = a<<1; // Left-Shift Operator by 1 will Multiply by 2  (value of b must be 12)
+        int c = a>>1; // Right-Shift Operator by 1 will Divide by 2 (value of c must be 3)
+        System.out.println("Value of b is : " + b);
+        System.out.println("Value of c is : " + c);
+    }
+}

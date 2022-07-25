7c693f95efdb82b60182f354496ee582d242347a
Parth Bhalerao <98455586+pvbgeek@users.noreply.github.com>
2022-07-25 20:24:40 +0530

    Add files via upload

---
+++ 004 Find i-th Bit of a Number/Main.java
@@ -0,0 +1,18 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int a = 6; // Number
+        int i = 2; // Position i
+        int mask = 1 << i;
+        int res = a & mask;
+        if(res != 0)
+        {
+            System.out.println("i-th Bit is 1.");
+        }
+        else
+        {
+            System.out.println("i-th Bit is 0.");
+        }
+    }
+}

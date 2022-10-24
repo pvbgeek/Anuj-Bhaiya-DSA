2404245ef8e811bccd744499647133b9a07f5c6c
Parth Bhalerao <98455586+pvbgeek@users.noreply.github.com>
2022-10-24 16:18:41 +0530

    Update Main.java

--- 004 Find i-th Bit of a Number/Main.java
+++ 004 Find i-th Bit of a Number/Main.java
@@ -3,7 +3,7 @@ public class Main
     public static void main(String[] args)
     {
         int a = 6; // Number
-        int i = 2; // Position i
+        int i = 2; // Position i (Position Starting from 0)
         int mask = 1 << i;
         int res = a & mask;
         if(res != 0)
